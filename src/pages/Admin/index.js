f5a569f40937a7520f3f30ce8be55f7b79c824cb
Antonio <antonio.muszato@gmail.com>
2025-01-19 23:13:00 -0400

    [FEAT] Função de concluir tarefa e manter logado feito

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,31 +1,31 @@
 import './admin.css';
 import { useState, useEffect } from 'react';
-import { addDoc, collection, onSnapshot, query, orderBy, where } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, query, orderBy, where, deleteDoc, doc, updateDoc } from 'firebase/firestore'
 import { db, auth } from '../../firebaseConnection';
 import { signOut } from 'firebase/auth';
 
-export default function Admin(){
+export default function Admin() {
 
     const [tarefaInput, setTarefaInput] = useState('');
     const [user, setUser] = useState({});
 
     const [tarefas, setTarefas] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         async function loadTarefas() {
             const userDetail = localStorage.getItem('@detailUser');
             setUser(JSON.parse(userDetail));
 
-            if(userDetail){
+            if (userDetail) {
                 const data = JSON.parse(userDetail);
 
                 const tarefaRef = collection(db, 'tarefas')
                 const q = query(tarefaRef, orderBy("created", 'desc'), where("userUid", "==", data?.uid))
 
-                const onsub = onSnapshot(q, (snapshot)=>{
+                const onsub = onSnapshot(q, (snapshot) => {
                     let lista = [];
 
-                    snapshot.forEach((doc)=>{
+                    snapshot.forEach((doc) => {
                         lista.push({
                             id: doc.id,
                             tarefa: doc.data().tarefa,
@@ -33,19 +33,16 @@ export default function Admin(){
                         })
                     })
                     setTarefas(lista);
-                    console.log(lista);
                 })
             }
         }
         loadTarefas();
     }, [])
 
-
-
     async function salvarTarefa(e) {
         e.preventDefault(); //nao atualizar a pagina para evitar o comportamento padrao do submit
 
-        if(tarefaInput === ''){
+        if (tarefaInput === '') {
             alert('Digite sua tarefa...')
             return;
         }
@@ -55,51 +52,56 @@ export default function Admin(){
             created: new Date(),
             userUid: user?.uid //"?" caso venha vazio ele nao vai crashar a aplicaçao
         })
-        .then(()=>{
-            setTarefaInput('');
-        })
-        .catch((err)=>{
-            alert(err);
-        })
+            .then(() => {
+                setTarefaInput('');
+            })
+            .catch((err) => {
+                alert(err);
+            })
     }
 
     async function logoutUser() {
         await signOut(auth);
     }
-    
-    return(
+
+    async function deletarTarefa(id) {
+        const docRef = doc(db, "tarefas", id)
+        await deleteDoc(docRef)
+    }
+
+    // async function editarTarefa(){
+    //     await updateDoc(collection(db, "tarefas"),{
+    //         tarefa: tarefas,
+    //     })
+    // }
+
+    return (
         <div className='admin-container'>
             <h1>Minhas tarefas</h1>
 
             <form className='form' onSubmit={salvarTarefa}>
 
                 <textarea placeholder='Digite sua tarefa...'
-                value={tarefaInput}
-                onChange={(e)=>setTarefaInput(e.target.value)}/>
+                    value={tarefaInput}
+                    onChange={(e) => setTarefaInput(e.target.value)} />
 
                 <button className='btn-register' type='submit'>Registrar tarefa</button>
 
             </form>
 
-            <article className='list'>
-                <p>
-                    aaa
-                </p>
-
-                <div>
-                    <button>Editar</button>
-                    <button className='btn-delete'>Concluir</button>
-                </div>
-
-            </article>
-
             {
-                tarefas.map((item)=>{
-                    return(
-                        <article key={item}>
+                tarefas.map((item) => {
+                    return (
+                        <article className='list' key={item.id}>
                             <p>
-                              {item.tarefa}
+                                {item.tarefa}
                             </p>
+
+                            <div>
+                                <button {/*onClick={editarTarefa}*/}>Editar</button>
+                                <button className='btn-delete' onClick={ () => deletarTarefa(item.id) }>Concluir</button>
+                            </div>
+
                         </article>
                     );
                 })
